ac822011a11923f378d0084a4db6d99dc4722622
Speros Misirlakis <6208387+keephopealive@users.noreply.github.com>
2024-08-26 12:48:10 -0700

    minor updates

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const http = require('node:http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+const server = http.createServer((request, response) => {
+    console.log('Node server received a request...\n');
+
+    if (request.url === '/') {
+        console.log('This is the "/" root route.\n');
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end('This is the "/" root route.\n');
+    }
+    else if (request.url === '/a') {
+        console.log('This is the "/a" route.\n');
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end('A\n');
+    }
+    else if (request.url === '/b') {
+        console.log('This is the "/b" route.\n');
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end('B\n');
+    }
+    else if (request.url === '/message') {
+        console.log('This is the "/message" route.\n');
+
+        fs.readFile('./message.html', function (errors, contents) {
+            if (errors) {
+                console.log(errors);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(contents);
+                response.end();
+            }
+        })
+    }
+
 });
 
 server.listen(port, hostname, () => {
